6169cee713b2dd14b078300dc4be009bce90e589
Saugat913 <kandelsaugat913@gmail.com>
2026-07-20 12:42:01 +0545

    chore: add the server singout too

--- wingbird-cli/src/cli/commands/logout.rs
+++ wingbird-cli/src/cli/commands/logout.rs
@@ -1,6 +1,8 @@
-use crate::{storage, ui::success};
+use crate::{api::ApiClient, storage, ui::success};
 
 pub async fn run(server_url: String) -> anyhow::Result<()> {
+    let api_client = ApiClient::from_storage(server_url.clone()).await?;
+    api_client.logout().await?;
     storage::delete_token(&server_url)?;
     success("Logged out successfully");
     Ok(())
